bf3e4fb6e03f44c7bb24fa9358b15f8040258ecb
MinhPhamNhat <minhnhatpham.0212@gmail.com>
2021-04-18 23:38:22 +0700

    51800903: Fix lỗi not found khi bấm vào đăng tin
    EDIT:
    /views
    + header.ejs
    /controllers
    +property

--- real-estate/app.js
+++ real-estate/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
+const cors = require('cors')
 
 const registerRouter = require('./controllers/register');
 const loginRouter = require('./controllers/login');
@@ -39,6 +40,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cors())
 
 app.use('/login', loginRouter);
 app.use('/logout', logoutRouter);
@@ -57,15 +59,15 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('404');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('404');
+});
 
 
 app.listen(PORT, ()=>console.log("http://"+HOST+":"+PORT))
