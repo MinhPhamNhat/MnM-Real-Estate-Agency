aac0748d48242bd3983314a318d6c206b28a1e24
Than-Trong-Huynh-Nhan <78633416+Than-Trong-Huynh-Nhan@users.noreply.github.com>
2021-05-01 12:29:49 +0700

    51800590: Cập nhập giao diện và thêm phần bất động sản theo địa điểm ở trang chủ

--- real-estate/controllers/index.js
+++ real-estate/controllers/index.js
@@ -7,7 +7,14 @@ const Statistic = require('../repository/StatisticRes')
 router.get('/', async(req, res, next) => {
     var properties = await Property.getBaseProperty({}, 0, 6, {date: -1})
     var getRange = await Statistic.getMinMaxRange()
-    res.render('index', { data: properties.data , range: getRange});
+    var propertiesByLocation = {
+        TP79: await Statistic.getNumberOfProperty({'location.cityId': 'TP79'}),
+        TP48: await Statistic.getNumberOfProperty({'location.cityId': 'TP48'}),
+        TP46: await Statistic.getNumberOfProperty({'location.cityId': 'TP46'}),
+        TP01: await Statistic.getNumberOfProperty({'location.cityId': 'TP01'}),
+        TP31: await Statistic.getNumberOfProperty({'location.cityId': 'TP31'})
+    }
+    res.render('index', { data: properties.data , range: getRange, propertiesByLocation});
 });
 
 module.exports = router;
